f3d8a9c8f88239c039b65c76d4b2334bedd45492
Nicholasaprileussukhadda <nicholas.aprileus@student.umn.ac.id>
2023-11-13 20:52:16 +0700

    index.blade

--- resources/views/layoutadmin/admin.blade.php
+++ resources/views/layoutadmin/admin.blade.php
@@ -1,11 +1,10 @@
-<!DOCTYPE html>
+{{-- <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>KopiPopi Dashboard</title>
-    <link href="{{ asset('css/admin.css') }}" rel="stylesheet">
 </head>
 <body>
     <h1>kopipopi dashboard</h1>
@@ -13,4 +12,4 @@
         @yield('content')
     </div>
 </body>
-</html>
+</html> --}}
