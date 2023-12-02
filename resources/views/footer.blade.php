cca2b318553aea9b8fd425b388f963ed26cd9e1d
sandya0 <66195253+sandya0@users.noreply.github.com>
2023-12-02 21:31:11 +0700

    scroll

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -21,6 +21,8 @@
     </style>
 </head>
 
+<section id="location">
+
 <body>
     <div class="footer-content" data-aos="fade-up" >
         <div class="footer-header">
@@ -97,5 +99,7 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"></script>
 </body>
+
+</section>
 
 </html>
--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -19,8 +19,8 @@
                     <div class="navbar-content">
                         <div class="dropbtn">Others &#9662;</div>
                         <div class="dropdown-content">
-                            <a href="#">Gallery</a>
-                            <a href="#">Location</a>
+                            <a href="#carousel">Gallery</a>
+                            <a href="#location">Location</a>
                             {{-- <a href="/login">Login</a> --}}
                             @auth
                                 <a href="/review">Review</a>
@@ -53,4 +53,22 @@
             });
         }
     });
+</script>
+
+<script>
+    // Smooth scrolling for anchor links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        const target = document.querySelector(this.getAttribute('href'));
+        if (target) {
+            window.scrollTo({
+                top: target.offsetTop,
+                behavior: 'smooth'
+            });
+        }
+    });
+});
+
 </script>
