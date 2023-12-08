bd119635982c69a0909d06d4a7a9ae9bc7dd4cf6
sandya0 <66195253+sandya0@users.noreply.github.com>
2023-12-08 21:53:05 +0700

    carousel

--- resources/views/carousel.blade.php
+++ resources/views/carousel.blade.php
@@ -1,43 +1,127 @@
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>IF-330 Cafetarian</title>
+    <link rel="stylesheet" type="text/css" href="css/navbar.css">
+    <link rel="stylesheet" type="text/css" href="css/index.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/aos@2.3.4/dist/aos.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;500;600&display=swap" rel="stylesheet">
+</head>
 <link rel="stylesheet" href="{{ asset('css/carousel.css') }}">
 
-<section id="carousel" class="carousel mb-5" data-aos="fade-right" data-aos-offset="500" data-aos-duration="2000">
-    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-        <ol class="carousel-indicators">
-            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-        </ol>
-        <h1 class="text-center mb-2">OUR GALLERY</h1>
+<!-- CSS for swiper -->
+<link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css" />
 
-        <div class="carousel-inner">
-            @foreach ($galleries as $index => $gallery)
-                <div class="carousel-item {{ $index == 0 ? 'active' : '' }}">
-                    <img class="d-block w-100" src="{{ asset('storage/' . $gallery->image) }}"
-                        alt="Gallery Image {{ $index + 1 }}">
-                    {{-- Your carousel caption content goes here --}}
-                </div>
-            @endforeach
+<!-- CSS for glightbox -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/glightbox/dist/css/glightbox.min.css">
+
+<!-- Your HTML structure -->
+<section id="carousel" class="carousel mb-5" data-aos="fade-right" data-aos-offset="500" data-aos-duration="2000">
+    <div class="swiper-container">
+        <div class="swiper-wrapper">
+            <div class="swiper-slide">
+                <a href="{{ asset('images/hero1.jpg') }}"  class="glightbox" data-glightbox="group:myGallery" data-gallery-id="myGallery">
+                    <img src="{{ asset('images/hero1.jpg') }}"  alt="First slide" data-gallery-id="myGallery">
+                </a>
+            </div>
+            <div class="swiper-slide">
+                <a href="{{ asset('images/menu2.png') }}" class="glightbox" data-glightbox="group:myGallery" data-gallery-id="myGallery">
+                    <img src="{{ asset('images/menu2.png') }}"  alt="Second slide" data-gallery-id="myGallery">
+                </a>
+            </div>
+            <div class="swiper-slide">
+                <a href="{{ asset('images/menu3.png') }}"  class="glightbox" data-glightbox="group:myGallery" data-gallery-id="myGallery">
+                    <img src="{{ asset('images/menu3.png') }}"  alt="Third slide" data-gallery-id="myGallery">
+                </a>
+            </div>
         </div>
 
-        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="sr-only">Previous</span>
-        </a>
-        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="sr-only">Next</span>
-        </a>
+        <div class="swiper-pagination"></div>
+        <!-- Add navigation buttons -->
+        <div class="swiper-button-prev"></div>
+        <div class="swiper-button-next"></div>
     </div>
 </section>
 
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
-    integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
+<!-- JS for glightbox -->
+<script src="https://cdn.jsdelivr.net/npm/glightbox/dist/js/glightbox.min.js"></script>
+
+<!-- JS for swiper -->
+<script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
+
+<script>
+    // Initialize Swiper
+    var swiper = new Swiper('.swiper-container', {
+        effect: 'fade',
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+    });
+
+    // Initialize glightbox
+    const lightbox = GLightbox({
+        selector: '.glightbox',
+        touchNavigation: true, // Enable swipe navigation in lightbox
+        loop: true, // Loop through images in lightbox
+        closeButton: true,
+        onOpen: (i, e) => {
+            const galleryItems = document.querySelectorAll('.swiper-slide');
+            galleryItems.forEach(item => {
+                item.classList.remove('selected-image');
+            });
+            e.activeSlide.classList.add('selected-image');
+        }
+    });
 </script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js"
-    integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous">
-</script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"
-    integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
-</script>
+
+
+
+
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.isotope/3.0.6/isotope.pkgd.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/aos@2.3.4/dist/aos.js"></script>
+
+    <script>
+        $(document).ready(function() {
+            // Initialize Isotope
+            $('.menu-container').isotope({
+                itemSelector: '.menu-item',
+                layoutMode: 'fitRows'
+            });
+
+            // Filter items on button click
+            $('#menu-flters li').on('click', function() {
+                $('#menu-flters li').removeClass('filter-active');
+                $(this).addClass('filter-active');
+                var selector = $(this).data('filter');
+                $('.menu-container').isotope({
+                    filter: selector
+                });
+            });
+        });
+
+        function scrollToSection(sectionId) {
+            var targetSection = document.querySelector(sectionId);
+
+            if (targetSection) {
+                targetSection.scrollIntoView({
+                    behavior: 'smooth'
+                });
+            }
+        }
+    </script>
+
+</html>
