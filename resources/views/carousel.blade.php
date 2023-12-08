d7bbfc3f8c0001d0fc2dc7ae4a22b5096c4ef7ba
Andaru Hymawan <130301294+AndaruHP@users.noreply.github.com>
2023-12-08 22:02:04 +0700

    carousel

--- resources/views/carousel.blade.php
+++ resources/views/carousel.blade.php
@@ -1,4 +1,5 @@
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,7 +8,8 @@
     <link rel="stylesheet" type="text/css" href="css/index.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/aos@2.3.4/dist/aos.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
+        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;500;600&display=swap" rel="stylesheet">
@@ -24,21 +26,15 @@
 <section id="carousel" class="carousel mb-5" data-aos="fade-right" data-aos-offset="500" data-aos-duration="2000">
     <div class="swiper-container">
         <div class="swiper-wrapper">
-            <div class="swiper-slide">
-                <a href="{{ asset('images/hero1.jpg') }}"  class="glightbox" data-glightbox="group:myGallery" data-gallery-id="myGallery">
-                    <img src="{{ asset('images/hero1.jpg') }}"  alt="First slide" data-gallery-id="myGallery">
-                </a>
-            </div>
-            <div class="swiper-slide">
-                <a href="{{ asset('images/menu2.png') }}" class="glightbox" data-glightbox="group:myGallery" data-gallery-id="myGallery">
-                    <img src="{{ asset('images/menu2.png') }}"  alt="Second slide" data-gallery-id="myGallery">
-                </a>
-            </div>
-            <div class="swiper-slide">
-                <a href="{{ asset('images/menu3.png') }}"  class="glightbox" data-glightbox="group:myGallery" data-gallery-id="myGallery">
-                    <img src="{{ asset('images/menu3.png') }}"  alt="Third slide" data-gallery-id="myGallery">
-                </a>
-            </div>
+            @foreach ($galleries as $gallery)
+                <div class="swiper-slide">
+                    <a href="{{ asset('storage/' . $gallery->image) }}" class="glightbox"
+                        data-glightbox="group:myGallery" data-gallery-id="myGallery">
+                        <img src="{{ asset('storage/' . $gallery->image) }}" alt="First slide"
+                            data-gallery-id="myGallery">
+                    </a>
+                </div>
+            @endforeach
         </div>
 
         <div class="swiper-pagination"></div>
@@ -88,40 +84,46 @@
 
 
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.isotope/3.0.6/isotope.pkgd.min.js"></script>
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/aos@2.3.4/dist/aos.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.isotope/3.0.6/isotope.pkgd.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+    integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
+</script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js"
+    integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous">
+</script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"
+    integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
+</script>
+<script src="https://cdn.jsdelivr.net/npm/aos@2.3.4/dist/aos.js"></script>
 
-    <script>
-        $(document).ready(function() {
-            // Initialize Isotope
-            $('.menu-container').isotope({
-                itemSelector: '.menu-item',
-                layoutMode: 'fitRows'
-            });
-
-            // Filter items on button click
-            $('#menu-flters li').on('click', function() {
-                $('#menu-flters li').removeClass('filter-active');
-                $(this).addClass('filter-active');
-                var selector = $(this).data('filter');
-                $('.menu-container').isotope({
-                    filter: selector
-                });
-            });
+<script>
+    $(document).ready(function() {
+        // Initialize Isotope
+        $('.menu-container').isotope({
+            itemSelector: '.menu-item',
+            layoutMode: 'fitRows'
         });
 
-        function scrollToSection(sectionId) {
-            var targetSection = document.querySelector(sectionId);
+        // Filter items on button click
+        $('#menu-flters li').on('click', function() {
+            $('#menu-flters li').removeClass('filter-active');
+            $(this).addClass('filter-active');
+            var selector = $(this).data('filter');
+            $('.menu-container').isotope({
+                filter: selector
+            });
+        });
+    });
 
-            if (targetSection) {
-                targetSection.scrollIntoView({
-                    behavior: 'smooth'
-                });
-            }
+    function scrollToSection(sectionId) {
+        var targetSection = document.querySelector(sectionId);
+
+        if (targetSection) {
+            targetSection.scrollIntoView({
+                behavior: 'smooth'
+            });
         }
-    </script>
+    }
+</script>
 
 </html>
