587eb5d07e7eaf0627fa113a0362d8876f22d6ff
sandya0 <66195253+sandya0@users.noreply.github.com>
2023-12-09 19:54:31 +0700

    Update menu.blade.php

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -17,7 +17,6 @@
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
     <link rel="stylesheet" href="{{ asset('css/menu.css') }}" />
-    <link rel="stylesheet" href="{{ asset('css/styles.css') }}" />
 
     <!-- Styles -->
     <style>
